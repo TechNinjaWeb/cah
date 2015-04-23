267bcca48ad3af8ff0ec1b0612e5035e6a24ebe3
phreelyfe <phreelyfe@gmail.com>
2015-04-23 06:03:37 -0300

    Routes and template pages setup

--- js/app/app.config.js
+++ js/app/app.config.js
@@ -4,12 +4,12 @@ app.config(function config($stateProvider, $urlRouterProvider, $locationProvider
             url: '',
             abstract: true,
             views: {
-                'nav': {
+                'nav@': {
                     templateUrl: "./js/app/html/template/app.nav.html",
                     controller: 'NavController'
                 },
-                'footer': {
-                    templateUrl: "./js/app/html/test/test.html",
+                'footer@': {
+                    templateUrl: "./js/app/html/template/app.footer.html",
                     controller: 'NavController'
                 }
             }
@@ -17,8 +17,8 @@ app.config(function config($stateProvider, $urlRouterProvider, $locationProvider
         .state('home.lobby', {
             url: '/',
             views: {
-                'content': {
-                	templateUrl: './js/app/html/pages/lobby.html',
+                'content@': {
+                	templateUrl: './js/app/html/pages/home.html',
                     controller: "LobbyController"
                 }
             }
@@ -33,20 +33,54 @@ app.config(function config($stateProvider, $urlRouterProvider, $locationProvider
             }
         })
         .state('game', {
-            url: '/game',
+            url: '',
             abstract: true,
             views: {
-                'content': {
-                	templateUrl: './js/app/html/template/game.output.html',
+                'nav@': {
+                    templateUrl: './js/app/html/template/game.nav.html',
+                    controller: ""
+                },
+                'content@': {
+                    templateUrl: './js/app/html/template/game.lobby.html',
+                    controller: ""
+                },
+                'left@': {
+                    templateUrl: './js/app/html/template/game.left.sidebar.html',
+                    controller: ""
+                },
+                'right@': {
+                    templateUrl: './js/app/html/template/game.right.sidebar.html',
+                    controller: ""
+                },
+                'footer@': {
+                    templateUrl: './js/app/html/template/app.footer.html',
                     controller: ""
                 }
             }
         })
-        .state('game.output', {
-            url: '',
+        .state('game.lobby', {
+            url: '/game',
             views: {
                 'game': {
-                	templateUrl: './js/app/html/test/test.html',
+                    templateUrl: './js/app/html/test/test.html',
+                    controller: ""
+                }
+            }
+        })
+        .state('game.one', {
+            url: '/level-one',
+            views: {
+                'game': {
+                    templateUrl: './js/app/html/test/game.test.html',
+                    controller: ""
+                }
+            }
+        })
+        .state('home.test', {
+            url: '/testing',
+            views: {
+                'content@': {
+                	template: 'test placeholder',
                     controller: ""
                 }
             }
